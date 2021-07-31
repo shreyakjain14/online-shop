98a452fd6a0f6683e7339576416aeb4037e72187
Shreyak Jain <shreyakjain14@gmail.com>
2021-07-31 22:23:12 +0530

    Online-shop

---
+++ routes/admin.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const express = require("express");
+const { body } = require("express-validator/check");
+
+const adminController = require("../controllers/admin");
+const isAuth = require("../middleware/is-auth");
+
+const router = express.Router();
+
+router.get("/add-product", isAuth, adminController.getAddProduct);
+
+router.get("/products", isAuth, adminController.getProducts);
+
+router.post(
+  "/add-product",
+  isAuth,
+  [
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  isAuth,
+  adminController.postAddProduct
+);
+
+router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
+
+router.post(
+  "/edit-product",
+  isAuth,
+  [
+    body('title')
+      .isString()
+      .isLength({ min: 3 })
+      .trim(),
+    body('price').isFloat(),
+    body('description')
+      .isLength({ min: 5, max: 400 })
+      .trim()
+  ],
+  adminController.postEditProduct
+);
+
+router.delete('/product/:productId', isAuth, adminController.deleteProduct);
+
+module.exports = router;
